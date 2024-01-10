c36d1ae09a90d075dcc12ac8cf2da400dbb79bdc
antisdun <112616257+antisdun@users.noreply.github.com>
2024-01-10 17:18:47 +0900

    chore: tailwind 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import LinkMain from "./pages/LinkMain";
+import QueryMain from "./pages/QueryMain";
 
 function App() {
   // const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="" element={<LinkMain />} />
+        <Route path="/query" element={<QueryMain />} />
       </Routes>
     </div>
   );
