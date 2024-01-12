a1101bba18a2bf4a782debaf0da939ecdc3e1177
yeskjv97 <yeskjv-97@hanmail.net>
2024-01-13 05:06:46 +0900

    'feat/#26'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import LinkMain from "./pages/LinkMain";
 import QueryMain from "./pages/QueryMain";
+import Search from "./pages/Search";
 
 function App() {
   // const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="" element={<LinkMain />} />
         <Route path="/query" element={<QueryMain />} />
+        <Route path="/Search" element={<Search />} />
       </Routes>
     </div>
   );
