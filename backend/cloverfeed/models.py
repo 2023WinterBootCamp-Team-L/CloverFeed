dd1e9e7b3f97173c8f4c250a63ae5f50bb780df9
AlgeMoya <kjhobd2@naver.com>
2024-01-15 17:29:14 +0900

    build: requirements 수정

--- backend/cloverfeed/models.py
+++ backend/cloverfeed/models.py
@@ -14,7 +14,7 @@ class AuthUser(AbstractUser):
 
 class FeedbackResult(models.Model):
     id = models.IntegerField(primary_key=True)
-    form_id = models.IntegerField()
+    form = models.ForeignKey("Form", on_delete=models.CASCADE)
     tag = models.CharField(max_length=255)
     respondent_name = models.CharField(max_length=255)
     category = models.CharField(max_length=255, blank=True, null=True)
@@ -28,7 +28,7 @@ class FeedbackResult(models.Model):
 
 class Form(models.Model):
     id = models.IntegerField(primary_key=True)  # 'id' 필드에 primary_key=True 속성 추가
-    user_id = models.IntegerField()
+    user = models.ForeignKey("AuthUser", on_delete=models.CASCADE)
     link = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField()
     modified_at = models.DateTimeField(blank=True, null=True)
@@ -39,7 +39,7 @@ class MultipleChoice(models.Model):
     id = models.CharField(
         primary_key=True, max_length=255
     )  # 'id' 필드에 primary_key=True 속성 추가
-    question_id = models.IntegerField()
+    question = models.ForeignKey("Question", on_delete=models.CASCADE)
     choice_context = models.CharField(max_length=255)
     select_limit = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField()
@@ -49,7 +49,7 @@ class MultipleChoice(models.Model):
 
 class Question(models.Model):
     id = models.IntegerField(primary_key=True)
-    form_id = models.IntegerField()
+    form = models.ForeignKey("Form", on_delete=models.CASCADE)
     context = models.CharField(max_length=255)
     type = models.CharField(max_length=3)
     created_at = models.DateTimeField()
@@ -59,8 +59,8 @@ class Question(models.Model):
 
 class QuestionAnswer(models.Model):
     id = models.IntegerField(primary_key=True)
-    feedback_id = models.IntegerField()
-    question_id = models.IntegerField()
+    feedback = models.ForeignKey("FeedbackResult", on_delete=models.CASCADE)
+    question = models.ForeignKey("Question", on_delete=models.CASCADE)
     context = models.TextField(blank=True, null=True)
     type = models.CharField(max_length=1, blank=True, null=True)
     created_at = models.DateTimeField()
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+APPEND_SLASH = False
 
 ROOT_URLCONF = "config.urls"
 
