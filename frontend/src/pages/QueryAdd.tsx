87ec891f4050e7522474c0540304da71dcd11c0f
antisdun <112616257+antisdun@users.noreply.github.com>
2024-01-15 16:01:02 +0900

    fix: Question 및 QuestionList 수정

--- frontend/src/pages/QueryAdd.tsx
+++ frontend/src/pages/QueryAdd.tsx
@@ -8,14 +8,29 @@ import { useQuestionContext } from "../components/QuestionUpdate";
 export interface QuestionProps {
   value: string;
   onTextChange: React.ChangeEventHandler<HTMLTextAreaElement>;
+  onFocus?: () => void;
+  onBlur?: () => void;
 }
 
-export const Question: React.FC<QuestionProps> = ({ value, onTextChange }) => {
+export const Question: React.FC<QuestionProps> = ({
+  value,
+  onTextChange,
+  onFocus,
+  onBlur,
+}) => {
+  const textColor =
+    value === "질문하고 싶은 내용을 입력하세요" ? "gray" : "black";
+  const textAreaStyle = {
+    color: textColor,
+  };
   return (
     <div>
       <textarea
         value={value}
         onChange={onTextChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        style={textAreaStyle}
         className="h-40 w-full border-c-green border-opacity-50 border-2 rounded-lg focus:outline-none leading-1.25 p-2 text-sm resize-none"
       />
     </div>
@@ -25,7 +40,20 @@ export const Question: React.FC<QuestionProps> = ({ value, onTextChange }) => {
 function QueryAdd() {
   const [showAnswersAdd, setShowAnswersAdd] = useState(false);
   const [answerInputs, setAnswerInputs] = useState<string[]>([]);
-  const [questionInputs, setQuestionInputs] = useState<string>("");
+  const [questionInputs, setQuestionInputs] =
+    useState<string>("질문하고 싶은 내용을 입력하세요");
+
+  const onFocus = () => {
+    if (questionInputs === "질문하고 싶은 내용을 입력하세요") {
+      setQuestionInputs("");
+    }
+  };
+
+  const onBlur = () => {
+    if (questionInputs === "") {
+      setQuestionInputs("질문하고 싶은 내용을 입력하세요");
+    }
+  };
 
   const { questions, setQuestions } = useQuestionContext();
 
@@ -38,15 +66,17 @@ function QueryAdd() {
   };
 
   const handleAddButtonClick = () => {
-    // 기존 질문 리스트에 새로운 질문 추가
-    const newQuestionList = [
-      ...questions,
-      { value: questionInputs, onTextChange: handleQuestionChange },
-    ];
-    setQuestions(newQuestionList);
+    if (questionInputs !== "질문하고 싶은 내용을 입력하세요") {
+      // 기존 질문 리스트에 새로운 질문 추가
+      const newQuestionList = [
+        ...questions,
+        { value: questionInputs, onTextChange: handleQuestionChange },
+      ];
+      setQuestions(newQuestionList);
 
-    // 입력된 질문 초기화
-    setQuestionInputs("");
+      // 입력된 질문 초기화
+      setQuestionInputs("");
+    }
   };
 
   const handleQuestionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -67,7 +97,12 @@ function QueryAdd() {
       <Toggle onChange={handleToggleChange} />
       <div className="flex flex-col gap-2">
         <p className="text-xl">질문 내용</p>
-        <Question value={questionInputs} onTextChange={handleQuestionChange} />
+        <Question
+          value={questionInputs}
+          onTextChange={handleQuestionChange}
+          onFocus={onFocus}
+          onBlur={onBlur}
+        />
       </div>
       {!showAnswersAdd && (
         <div className="flex flex-col gap-2">
