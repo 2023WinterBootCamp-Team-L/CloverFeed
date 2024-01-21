5920890ac11129ef7a8a9ee565c933d52dd0851d
antisdun <112616257+antisdun@users.noreply.github.com>
2024-01-21 21:32:38 +0900

    feat: 작성한 질문 목록 확인 API

--- frontend/src/pages/LinkAnswer1.tsx
+++ frontend/src/pages/LinkAnswer1.tsx
@@ -1,6 +1,81 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import BackButton from "../components/BackButton";
 
+interface Question {
+  context: string;
+  type: "option" | "short";
+  choices?: string;
+}
+
+interface ApiResponse {
+  status: "success" | "error";
+  questions?: Question[];
+  error_code?: number;
+  message?: string;
+}
+
 function LinkAnswer1() {
+  // const dummyQuestions: Question[] = [
+  //   {
+  //     context: "당신의 직무는 무엇인가요?",
+  //     type: "option",
+  //     choices: "개발자",
+  //   },
+  //   {
+  //     context: "님의 업무 능력 강점은 무엇인가요?",
+  //     type: "option",
+  //     choices: "박학다식",
+  //   },
+  //   {
+  //     context: "님의 성격 및 태도는 어떤가요?",
+  //     type: "option",
+  //     choices: "책임감",
+  //   },
+  //   {
+  //     context: "님에게 전하고 싶은 칭찬이 있나요?",
+  //     type: "short",
+  //   },
+  //   {
+  //     context: "님이 보완해 줬으면 하는 부분이 있나요?",
+  //     type: "short",
+  //   },
+  // ];
+
+  const [questions, setQuestions] = useState<Question[] | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<ApiResponse>(
+          "http://localhost:8000/api/form/questions/?",
+          {
+            params: {
+              userid: 1,
+            },
+          }
+        );
+        // const response: ApiResponse = {
+        //   status: "success",
+        //   questions: dummyQuestions,
+        // };
+
+        if (response.data.status === "success") {
+          console.log("피드백 폼 질문");
+          setQuestions(response.data.questions);
+        } else {
+          console.error(
+            `Error: ${response.data.error_code}, ${response.data.message}`
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="flex justify-center items-center h-screen ">
       <div
@@ -11,20 +86,21 @@ function LinkAnswer1() {
           <BackButton back page="/LinkTag2" />
           <BackButton back={false} page="/LinkAnswer2" />
         </div>
-        <div className="flex-col flex-full justify-between pt-12 pb-12">
-          <p className="text-left font-Preahvihear text-24 text-xl font-normal font-weight-400 ">
-            XXX님에게 전하고 싶은
-          </p>
-          <p className="text-left font-Preahvihear text-24 text-xl font-normal font-weight-400 ">
-            칭찬이 있나요?
-          </p>
-        </div>
-        <div className="flex flex-1 flex-col justify-center items-center">
-          <div className="w-[360px] h-[580px] flex flex-col items-start overflow-hidden relative border-2 rounded-lg border-[#C1C6CF] bg-white">
-            <p className="text-left font-Preahvihear text-[14px] font-normal mt-4 ml-4">
-              내용을 적어주세요
-            </p>
-          </div>
+        <div>
+          {questions !== undefined ? (
+            <ul>
+              {questions.map((question, index) => (
+                <li key={index}>
+                  <p>{question.context}</p>
+                  <p>질문내용</p>
+                  {question.choices && <p>Choices: {question.choices}</p>}
+                  <p>질문 답변</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
       </div>
     </div>
