dd39c85515255ff6624c2ab36b556d38f3f8bdc8
antisdun <112616257+antisdun@users.noreply.github.com>
2024-01-27 00:13:53 +0900

    fix: 로그인페이지에 피드백폼 유무 조회 api 연동

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,7 +2,6 @@ import loginclover from "../assets/loginclover.svg";
 import GreenButton from "../components/GreenButton";
 
 function Login() {
-  const nextpage = "/Mainpage";
   return (
     <div
       className="bg-white flex flex-col mx-auto h-screen gap-20 px-5 py-[137px]"
@@ -11,7 +10,7 @@ function Login() {
       <p className="font-pre text-[24px] font-bold text-center">Welcome</p>
       <img src={loginclover} alt="로그인클로버" />
       <div className="flex justify-center items-center">
-        <GreenButton text="나의 피드백 확인하러 가기" nextpage={nextpage} />
+        <GreenButton text="나의 피드백 확인하러 가기" nextpage="/Mainpage" />
       </div>
     </div>
   );
