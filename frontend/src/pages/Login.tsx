f025b929e0f95b5fd2912ca4c1f8344672d5abc7
yeskjv97 <yeskjv-97@hanmail.net>
2024-02-01 16:16:29 +0900

    query페이지들 반응형 수정

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,16 +4,17 @@ import GreenButton from "../components/GreenButton";
 function Login() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div
-        className="bg-white flex flex-col mx-auto min-h-screen gap-20 px-5 py-20 overflow-hidden w-full sm:w-[393px] lg:w-[393px]"
-        // style={{ width: '393px' }}
-      >
+    <div
+      className="bg-white flex flex-col mx-auto min-h-screen gap-20 px-5 py-20 overflow-hidden w-full sm:w-[393px] lg:w-[393px]"
+      // style={{ width: '393px' }}
+    >
+      <div className="w-full h-full flex flex-1 flex-col justify-center items-center">
         <p className="font-pre text-[24px] font-bold text-center">Welcome</p>
         <img src={loginclover} alt="로그인클로버" />
         <div className="flex justify-center items-center">
           <GreenButton text="나의 피드백 확인하러 가기" nextpage="/Mainpage" />
         </div>
-      </div>
+      </div></div>
     </div>
   );
 }
--- frontend/src/pages/QueryStart.tsx
+++ frontend/src/pages/QueryStart.tsx
@@ -4,25 +4,31 @@ import XButton from "../components/XButton";
 
 function QueryStart() {
   return (
-    <div className="bg-gradient-to-t from-c-emerald">
-      <div className="w-full sm:max-w-[393px] lg:max-w-[393px]  flex flex-col mx-auto min-h-screen gap-10 px-5 py-8">
-        <div className="flex flex-row justify-end">
-          <XButton nextpage="/MainPage" />
-        </div>
-        <div className="flex flex-col gap-14">
-          <div>
-            <p className="font-pre text-[22px] font-bold text-center">
-              나만의 질문을 생성해서
-            </p>
-            <p className="font-pre text-[22px] font-bold text-center">
-              <span className="text-c-green font-bold">동료에게 피드백</span>을
-              받아봐요!
-            </p>
+    <div className="flex justify-center items-center min-h-screen">
+      <div
+        //sm은 화면 너비 640px이상(모바일), md는 768px이상(테블릿), lg는 1024px이상(데스크탑)
+        className="flex px-5 py-10 flex-col  items-center relative bg-c-emerald bg-opacity-35 overflow-hidden min-h-screen w-full sm:w-[393px] lg:w-[393px]"
+        //style={{ width: '393px', height: '852px' }}
+      >
+        <div className=" w-full h-full flex flex-1 flex-col justify-center items-center">
+          <div className="w-full h-full  flex flex-1  flex-start justify-end">
+            <XButton nextpage="/MainPage" />
           </div>
-          <div className="flex flex-col items-center">
+          <div>
+            <div>
+              <p className="font-pre text-[22px] font-bold text-center">
+                나만의 질문을 생성해서
+              </p>
+              <p className="font-pre text-[22px] font-bold text-center">
+                <span className="text-c-green font-bold">동료에게 피드백</span>
+                을 받아봐요!
+              </p>
+            </div>
+          </div>
+          <div className=" w-full h-full flex flex-1 flex-col justify-center items-center">
             <img src={querystart} alt="쿼리시작" />
           </div>
-          <div className="flex justify-center items-center">
+          <div className=" w-full h-full flex flex-1 flex-col justify-center items-center">
             <GreenButton text="질문 폼 생성하기" nextpage="/QueryList" />
           </div>
         </div>
