f02e9c8a834ed38534338fdce4589cbea81213d6
HseoHan <hanhs3925@gmail.com>
2024-04-23 01:01:30 +0900

    로그인 회원가입

--- frontend/src/components/ResearchButton.tsx
+++ frontend/src/components/ResearchButton.tsx
@@ -1,24 +1,27 @@
-import { useNavigate } from 'react-router-dom';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface ResearchButtonProps {
-  iconSrc: string; // 이미지 소스는 문자열로 표현
   researchpage: string;
 }
 
-const ResearchButton: React.FC<ResearchButtonProps> = ({
-  iconSrc,
-  researchpage,
-}) => {
-  const navigate = useNavigate();
+const ResearchButton: React.FC<ResearchButtonProps> = ({ researchpage }) => {
+  const router = useRouter();
 
   const handleButtonClick = () => {
-    navigate(researchpage);
+    router.push(researchpage);
   };
 
   return (
     <button onClick={handleButtonClick}>
       <div className="flex flex-col justify-center gap-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-        <img src={iconSrc} alt="Research Icon" className="h-[40px]" />
+        <Image
+          src="/common/researchicon.svg"
+          width={50}
+          height={50}
+          alt="Research Icon"
+          className="h-[40px] ml-1.5"
+        />
         <p className="font-pre text-[14px] font-bold">피드백 검색</p>
       </div>
     </button>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,27 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     backgroundImage: {
-      clover: "url(./assets/clovercloud.svg)",
+      clover: "url(/common/clovercloud.svg)",
     },
     extend: {
       colors: {
-        'c-green': '#50DA8C',
-        'c-indigo': '#1E2C49',
-        'c-pink': '#F9C7C7',
-        'c-l-pink': '#FBE2E2', // opacity: 50
-        'c-yellow': '#F6EED4',
-        'c-emerald': '#D5FBE5',
-        'c-blue': '#E2E9FF',
-        'c-l-blue': '#F3F6FF', // opacity: 40
-        'c-purple': '#EDD0F5',
-        'c-sl-purple': '#F5E7F9', // opacity: 50
-        'c-l-purple': '#FAF1FC', // opacity: 70
-        'c-gray': '#EEEFF0',
+        "c-green": "#50DA8C",
+        "c-indigo": "#1E2C49",
+        "c-pink": "#F9C7C7",
+        "c-l-pink": "#FBE2E2", // opacity: 50
+        "c-yellow": "#F6EED4",
+        "c-emerald": "#D5FBE5",
+        "c-blue": "#E2E9FF",
+        "c-l-blue": "#F3F6FF", // opacity: 40
+        "c-purple": "#EDD0F5",
+        "c-sl-purple": "#F5E7F9", // opacity: 50
+        "c-l-purple": "#FAF1FC", // opacity: 70
+        "c-gray": "#EEEFF0",
       },
       fontFamily: {
-        pre: ['Pretendard'],
+        pre: ["Pretendard"],
       },
     },
   },
