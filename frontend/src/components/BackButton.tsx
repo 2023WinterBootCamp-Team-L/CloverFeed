45fd02aa7fdc9e42a56fbf1ff0dd5a679de0e850
antisdun <112616257+antisdun@users.noreply.github.com>
2024-04-29 04:06:15 +0900

    refactor: 검색 페이지 리팩토링

--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -1,6 +1,5 @@
-import { useNavigate } from 'react-router-dom';
-import nextarrow from '../assets/nextarrow.svg';
-import backarrow from '../assets/backarrow.svg';
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 interface BackButtonProps {
   back?: boolean;
@@ -9,14 +8,14 @@ interface BackButtonProps {
 }
 
 const BackButton = ({ page, back = true, onClick }: BackButtonProps) => {
-  const navigate = useNavigate();
+  const router = useRouter();
 
   const handleButtonClick = () => {
     if (onClick) {
       onClick();
     }
     if (page) {
-      navigate(page);
+      router.push(page);
     }
   };
 
@@ -24,14 +23,17 @@ const BackButton = ({ page, back = true, onClick }: BackButtonProps) => {
     <button
       className={`${
         back
-          ? 'bg-white border-c-indigo border-[0.1rem]'
-          : 'bg-c-indigo border-c-indigo border-[0.1rem]'
+          ? "bg-white border-c-indigo border-[0.1rem]"
+          : "bg-c-indigo border-c-indigo border-[0.1rem]"
       } rounded-full h-9 w-9 min-w-7 min-h-7 flex items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300`}
       onClick={handleButtonClick}
     >
-      <img
-        src={back ? backarrow : nextarrow}
-        className={`${back ? 'mr-1' : 'ml-1'} h-5 w-5`}
+      <Image
+        src={back ? "/common/backarrow.svg" : "/common/nextarrow.svg"}
+        width={50}
+        height={50}
+        alt={back ? "뒤로가기" : "다음으로"}
+        className={`${back ? "mr-1" : "ml-1"} h-5 w-5`}
       />
     </button>
   );
